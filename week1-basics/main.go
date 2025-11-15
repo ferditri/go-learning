d49d68cf3a37c82c2b6f5621e0b927c0f756109c
Ferdiyanto Tri Setiawan <ferditrisetiawan29@gmail.com>
2025-11-15 10:20:05 +0700

    Added simple math in main.go

--- week1-basics/main.go
+++ week1-basics/main.go
@@ -20,4 +20,19 @@ func main() {
 	fmt.Println("Sudah Bekerja:\t", sudahBekerja)
 	fmt.Println("Alamat:\t", alamat)
 
+	// day 2
+
+	angka1 := 5
+	angka2 := 5
+
+	perkalian := angka1 * angka2
+	pembagian := angka1 / angka2
+	penambahan := angka1 + angka2
+	pengurangan := angka1 - angka2
+
+	fmt.Println(perkalian)
+	fmt.Println(pembagian)
+	fmt.Println(penambahan)
+	fmt.Println(pengurangan)
+
 }
