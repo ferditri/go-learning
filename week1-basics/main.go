96fd4598d6fa7fcccb1b8386d82d3aa514139ff9
Ferdiyanto Tri Setiawan <ferditrisetiawan29@gmail.com>
2025-11-15 11:05:36 +0700

    Add temperature conversion

--- week1-basics/main.go
+++ week1-basics/main.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 
-	// day 1
+	// latihan 1
 
 	namaLengkap := "Ferdiyanto Tri Setiawan"
 	NPM := 25082010135
@@ -20,7 +20,7 @@ func main() {
 	fmt.Println("Sudah Bekerja:\t", sudahBekerja)
 	fmt.Println("Alamat:\t", alamat)
 
-	// day 2
+	// latihan 2
 
 	angka1 := 5
 	angka2 := 5
@@ -30,9 +30,22 @@ func main() {
 	penambahan := angka1 + angka2
 	pengurangan := angka1 - angka2
 
-	fmt.Println(perkalian)
-	fmt.Println(pembagian)
-	fmt.Println(penambahan)
-	fmt.Println(pengurangan)
+	fmt.Println("Perkalian: ", perkalian)
+	fmt.Println("Pembagian: ", pembagian)
+	fmt.Println("Penambahan: ", penambahan)
+	fmt.Println("Pengurangan: ", pengurangan)
+
+	// latihan 3
+
+	var celcius, fahrenheit, kelvin float64
+
+	celcius = 18
+
+	var C = celcius
+	fahrenheit = (C * 9 / 5) + 32
+	kelvin = C + 273.15
+
+	fmt.Println("Fahrenheit: ", fahrenheit)
+	fmt.Println("Kelvin: ", kelvin)
 
 }
