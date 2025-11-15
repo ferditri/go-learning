6469f1ed133cdfa57ca620e34a4925b6f52d9790
Ferdiyanto Tri Setiawan <ferditrisetiawan29@gmail.com>
2025-11-15 09:56:44 +0700

    Initial commit

---
+++ week1-basics/go.mod
@@ -0,0 +1,3 @@
+module week1-basics
+
+go 1.24.5
---
+++ week1-basics/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// day 1
+
+	namaLengkap := "Ferdiyanto Tri Setiawan"
+	NPM := 25082010135
+	jurusan := "Sistem Informasi"
+	semester := 1
+	sudahBekerja := false
+
+	fmt.Println("Nama Lengkap saya:\t", namaLengkap)
+	fmt.Println("NPM saya:\t", NPM)
+	fmt.Println("Jurusan saya:\t", jurusan)
+	fmt.Println("Saya semester:\t", semester)
+	fmt.Println("Sudah Bekerja:\t", sudahBekerja)
+
+}
